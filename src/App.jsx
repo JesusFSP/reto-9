1d20ec8af1cf4434278542e7bf3c37df6c5d376d
JesusFSP <jsilva@unsa.edu.pe>
2023-07-03 05:04:57 -0500

    Creando el proyecto del reto 9

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-sky-950">Rick and Morty</h1>
+    </>
+  )
+}
+
+export default App
